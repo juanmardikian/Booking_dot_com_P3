613304416befb48ee730197193a74aa9d5c106a6
Juan Mardikian <user.username>
2020-02-16 15:25:44 -0500

    icons on the right side

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,12 +6,13 @@ import iconSearch from '../images/iconSearch.png'
 export default function Header() {
     return (
         <div className="header">
-
-            <span className='Booking title'>Booking</span><span className='com title'>.com</span>
-
-            <img className="icons iconPhone" alt='' src={iconPhone}></img>
-            <img className="icons iconProfile" alt='' src={iconProfile}></img>
-            <img className="icons iconSearch" alt='' src={iconSearch}></img>
+<div className='title'><span className='Booking'>Booking</span><span className='com'>.com</span></div>
+<div className="icons">
+    <div><img className="iconPhone" alt='' src={iconPhone}></img></div>
+    <div><img className="iconProfile" alt='' src={iconProfile}></img></div>
+    <div><img className="iconSearch" alt='' src={iconSearch}></img></div>
+</div>
+       
 
         </div>
     )
