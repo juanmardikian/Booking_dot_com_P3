7b1cbc6d00931db993891c208f01be30e2d8a20d
Michael Ip <michaelip@Michaels-Air.home>
2020-02-17 20:39:40 -0500

    minor changes to routes, plus laid groundwork for restrict

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ router.get('/location/:id', controllers.getLocation);
 
 router.get('/user/:id', controllers.getUser);
 router.delete('/user/:id', controllers.deleteUser);
-// router.post('/user', controllers.createUser);
 router.put('/user/:id', controllers.updateUser);
 
 router.get('/trip/:id', controllers.getTrip);
@@ -23,12 +22,14 @@ router.get('/location/:id/cars', controllers.getCars);
 router.get('/location/:id/hotels', controllers.getHotels);
 router.get('/location/:id/volunteers', controllers.getVolunteers);
 
-router.post('/sign-up', controllers.signUp)
-router.post('/sign-in', controllers.signIn)
-router.post('/change-password', controllers.changePassword)
-// router.post('/items', restrict, controllers.createItem)
-// router.put('/items/:id', restrict, controllers.updateItem)
-// router.delete('/items/:id', restrict, controllers.deleteItem)
-// router.get('/users/:id/items', restrict, controllers.getUserItems)
+router.post('/sign-up', controllers.signUp);
+router.post('/sign-in', controllers.signIn);
+router.post('/change-password', controllers.changePassword);
+
+// add restrict to changePassword and updateUser
+// router.post('/trip', restrict, controllers.createTrip)
+// router.put('/trip/:id', restrict, controllers.updateTrip)
+// router.delete('/trip/:id', restrict, controllers.deleteTrip)
+// router.get('/trip/:id', restrict, controllers.getTrip)
 
 module.exports = router
